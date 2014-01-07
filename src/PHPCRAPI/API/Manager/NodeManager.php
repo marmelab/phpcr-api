4460625cc0c7f5e4dab354daf10aec6fc764d3b0
Robin Bressan <robin@buddey.net>
2014-01-07 09:59:07 +0100

    Add rename and remove for NodeManager

--- src/PHPCRAPI/API/Manager/NodeManager.php
+++ src/PHPCRAPI/API/Manager/NodeManager.php
@@ -11,6 +11,7 @@ namespace PHPCRAPI\API\Manager;
 
 use PHPCR\ItemNotFoundException;
 use PHPCR\AccessDeniedException as PHPCRAccessDeniedException;
+use PHPCR\ItemExistsException;
 use PHPCR\RepositoryException;
 use PHPCR\UnsupportedRepositoryOperationException;
 use PHPCR\Version\VersionException;
@@ -155,6 +156,49 @@ class NodeManager
             throw new InternalServerErrorException('This node is versionable and checked-in or is non-versionable but its nearest 
                 versionable ancestor is checked-in and this implementation performs this validation immediately instead of waiting 
                 until save');
+        }catch(RepositoryException $e){
+            throw new InternalServerErrorException($e->getMessage());
+        }
+    }
+
+    public function rename($newName)
+    {
+        if(is_null($newName) || mb_strlen($newNname) == 0){
+            throw new InternalServerErrorException('The new name is empty');
+        }
+
+        try{
+            $this->node->rename($newName);
+            $this->sessionManager->save();
+        }catch(ItemExistsException $e){
+            throw new InternalServerErrorException('There already exists a sibling node of this node with the specified name, same-name siblings are not 
+                allowed and this implementation performs this validation immediately');
+        }catch(LockException $e){
+            throw new ResourceLockedException('A lock prevents the name change and this implementation performs this validation immediately');
+        }catch(ConstraintViolationException $e){
+            throw new ResourceConstraintViolationException('A node type or implementation-specific constraint is violated and this implementation performs this validation immediately');
+        }catch(VersionException $e){
+            throw new InternalServerErrorException('This node is read-only due to a checked-in node and this implementation performs this validation immediately');
+        }catch(RepositoryException $e){
+            throw new InternalServerErrorException($e->getMessage());
+        }
+    }
+
+    public function remove()
+    {
+        try{
+            $this->node->remove();
+            $this->sessionManager->save();
+        }catch(PHPCRAccessDeniedException $e){
+            throw new AccessDeniedException('This node or an node in its subgraph is currently the target of a REFERENCE property located in this workspace but 
+                outside this item\'s subgraph and the current Session does not have read access to that REFERENCE property or if the current 
+                Session does not have sufficient privileges to remove the node');
+        }catch(LockException $e){
+            throw new ResourceLockedException('A lock prevents the removal of this node and this implementation performs this validation immediately instead of waiting until save');
+        }catch(ConstraintViolationException $e){
+            throw new ResourceConstraintViolationException('Removing the specified node would violate a node type or implementation-specific constraint and this implementation performs this validation immediately instead of waiting until save');
+        }catch(VersionException $e){
+            throw new InternalServerErrorException('The parent node of this node is versionable and checked-in or is non-versionable but its nearest versionable ancestor is checked-in and this implementation performs this validation immediately instead of waiting until save');
         }catch(RepositoryException $e){
             throw new InternalServerErrorException($e->getMessage());
         }
