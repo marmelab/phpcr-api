1bf0a2f78b229c4b7095553560c2b38b263cf847
Robin Bressan <robin@buddey.net>
2014-06-05 14:06:55 +0200

    Add tests for RepositoryManager

---
+++ src/PHPCRAPI/API/Tests/Manager/RepositoryManagerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHPCRAPI\API\Tests\Manager;
+
+use PHPCRAPI\API\Manager\RepositoryManager;
+use PHPCRAPI\API\Manager\SessionManager;
+
+class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
+{
+    use \Xpmock\TestCaseTrait;
+
+    public function testItShouldReturnASessionManager() {
+        $sessionInterface = $this->mock('\PHPCR\SessionInterface', null);
+        $repositoryInterface = $this->mock('\PHPCR\RepositoryInterface', null);
+
+        $repository = $this->mock('\PHPCRAPI\PHPCR\Repository')
+            ->login($sessionInterface)
+            ->getRepository($repositoryInterface)
+            ->getParameters([
+                'credentials.username' => 'admin',
+                'credentials.password' => 'admin'
+            ])
+            ->new();
+
+        $manager = new RepositoryManager($repository);
+        $this->assertTrue($manager->getSessionManager() instanceof SessionManager);
+    }
+}
